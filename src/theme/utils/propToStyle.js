7360cb22a82c5ce0eddaf8f6470fd73d8dfe33a8
Felipe Wanner <felipe.w.pakua@gmail.com>
2022-03-31 11:40:35 -0300

    adding propToStyle function, function used to make the addition of css elements the most generic as possible

---
+++ src/theme/utils/propToStyle.js
@@ -0,0 +1,38 @@
+import { breakpointsMedia } from "./breakpointsMedia"
+// funcao que converte um "propName" que nós passamos. E basicamente ele faz o retorno de um objeto aonde propName é 
+// uma propriedade css (textAlign, border, margin, etc), e "props" tem todas as propriedades que passamos quando chamamos a
+// funcao "Text" (que chama "textBase", onde "propToStyle" é chamado)
+export default function propToStyle(propName) {
+  return function(props){
+    // console.log("chave propname",[propName])
+    // console.log("chave props", props[propName])
+
+    const propValue = props[propName] // pode receber string ou objeto, porém as propriedades css aceitam apenas strings,
+                                      // então precisamos tratar isso com um if
+    if(typeof propValue === "string"){
+      return {
+        [propName]: propValue
+      }
+    }
+    if(typeof propValue === "object"){
+      return breakpointsMedia({
+        xs: {
+          [propName]: propValue.xs
+        },
+        sm: {
+          [propName]: propValue.sm
+        },
+        md: {
+          [propName]: propValue.md
+        },
+        lg: {
+          [propName]: propValue.lg
+        },
+        xl: {
+          [propName]: propValue.xl
+        },
+      })
+      
+    }
+  }
+}
